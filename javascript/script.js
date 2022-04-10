b929d6dd1ac5d1121918c160e9fe6a74ebbcc079
Ennio-M <marrese.ennio@gmail.com>
2022-04-11 00:27:18 +0200

    esercizio completato

--- javascript/script.js
+++ javascript/script.js
@@ -28,70 +28,82 @@ const items = [
 ]
 
 // Inizializzazione variabili
-let itemTemplate = "";
-let thumbTemplate = "";
 let current = 0;
 
 // Generazione pagina
 generatePage();
+let loopFunction = setInterval(next, 3000);
 
 // Eventi
-const up = document.querySelector(".fa-circle-chevron-up");
-const down = document.querySelector(".fa-circle-chevron-down");
-up.addEventListener("click", previous);
-down.addEventListener("click", next);
+const stopButton = document.getElementById("stop");
+const resume = document.getElementById("resume");
+stopButton.addEventListener("click", stopLoop);
+resume.addEventListener("click", resumeFunction);
 
 // Funzioni
 // Funzione per generare la pagina
 function generatePage() {
+    const itemsContainer = document.querySelector(".items-container");
+    const thumbsContainer = document.querySelector(".thumbs-container");
     for(let i = 0; i < items.length; i++) {
         let active = "";
         if(i === current) {
             active = "active";
         }
-        itemTemplate += `
-        <div class="item ${active}">
-            <img src="${items[i]['image']}" alt="${items[i]['title']}">
-            <div class="title">
-                <h2>${items[i]['title']}</h2>
-                <p>${items[i]['text']}</p>
-            </div>
-        </div>`;
-        thumbTemplate += `
-        <div class="thumb ${active}">
-            <img src="${items[i]['image']}" alt="${items[i]['title']}">
-        </div>`
+        const carouselImage = addImage(i, active);
+        itemsContainer.append(carouselImage[0]);
+        thumbsContainer.append(carouselImage[1]);
     }
-    let itemsContainer = document.querySelector(".items-container");
-    itemsContainer.innerHTML = itemTemplate;
-    let thumbsContainer = document.querySelector(".thumbs-container");
-    thumbsContainer.innerHTML += thumbTemplate;
 }
-// Funzione per mandare indietro le immagini
-function previous() {
-    const imgs = document.getElementsByClassName("item");
-    imgs[current].classList.remove("active");
-    const thumbs = document.getElementsByClassName("thumb");
-    thumbs[current].classList.remove("active");
-    if(current === 0) {
-        current = 4;
-    } else {
-        current--;
-    }
-    imgs[current].classList.add("active");
-    thumbs[current].classList.add("active");
+// Funzione per generare le immagini
+function addImage(i, active) {
+    const item = document.createElement("div");
+    const thumb = document.createElement("div");
+    const imgItem = document.createElement("img");
+    const imgThumb = document.createElement("img");
+    const title = document.createElement("div");
+    const h2 = document.createElement("h2");
+    const p = document.createElement("p");
+    item.setAttribute("class", `item ${active}`);
+    thumb.setAttribute("class", `thumb ${active}`);
+    imgItem.setAttribute("src", `${items[i]['image']}`);
+    imgItem.setAttribute("alt", `${items[i]['title']}`);
+    imgThumb.setAttribute("src", `${items[i]['image']}`);
+    imgThumb.setAttribute("alt", `${items[i]['title']}`);
+    title.setAttribute("class", "title");
+    h2.innerHTML = `${items[i]['title']}`;
+    p.innerHTML = `${items[i]['text']}`;
+    title.append(h2, p);
+    item.append(imgItem, title);
+    thumb.append(imgThumb);
+    return [item, thumb];
 }
+
 // Funzione per mandare avanti le immagini
 function next() {
-    const images = document.getElementsByClassName("item");
-    images[current].classList.remove("active");
-    const thumbs = document.getElementsByClassName("thumb");
-    thumbs[current].classList.remove("active");
+    const elements = removeActive();
     if(current === 4) {
         current = 0;
     } else {
         current++;
     }
-    images[current].classList.add("active");
-    thumbs[current].classList.add("active");
+    elements[0][current].classList.add("active");
+    elements[1][current].classList.add("active");
+}
+// Funzione per rimuovere la classe active
+function removeActive() {
+    const images = document.getElementsByClassName("item");
+    images[current].classList.remove("active");
+    const thumbs = document.getElementsByClassName("thumb");
+    thumbs[current].classList.remove("active");
+    return [images, thumbs];
+}
+
+// Funzione per fermare il loop
+function stopLoop() {
+    clearInterval(loopFunction);
+}
+// Funzione per riprendere il loop
+function resumeFunction() {
+    loopFunction = setInterval(next, 3000);
 }
